097e6a36bcdfedfcd71d4bdde6cb23b1d0da9b49
nachod619 <ignacio.delgadov@utem.cl>
2022-07-15 21:37:21 -0400

    certificado ssl
    -certificado agregado en assets
    - codigo agregado al inicio del main

--- appcpyd/lib/main.dart
+++ appcpyd/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:appcpyd/homepage.dart';
 import 'package:appcpyd/routes/routes.dart';
 import 'package:appcpyd/vistas/loginalumno/controllers/lcontroller.dart';
@@ -5,12 +7,17 @@ import 'package:appcpyd/vistas/loginalumno/l_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-//import 'package:http/http.dart' as http;
-//import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ByteData data = await PlatformAssetBundle().load('assets/ca/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext.setTrustedCertificatesBytes(data.buffer.asUint8List());
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -25,10 +32,10 @@ class MyApp extends StatelessWidget {
       initialBinding: BindingsBuilder(() {
         Get.put(logincontroller());
       }),
-      title: 'APP CPYD',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      //title: 'APP CPYD',
+      //theme: ThemeData(
+        //primarySwatch: Colors.blue,
+      //),
       //home: pantalla_login(),
       home: HomePage(),
     );
